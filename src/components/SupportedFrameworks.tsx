a0cde01d942218c401ce0692452ab75faad3bc53
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-04-29 05:03:03 +0100

    add fade-in animation for framework logos

--- src/components/SupportedFrameworks.tsx
+++ src/components/SupportedFrameworks.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+'use client'
+
+import React from 'react';
+import { useInView } from "react-intersection-observer";
 import '../styles/SupportedFrameworks.css'
 import jaxLogo from 'src/assets/jax-logo.png'
 import numpyLogo from 'src/assets/numpy-logo.png'
@@ -7,22 +10,29 @@ import torchLogo from 'src/assets/torch-full-logo.png'
 import tfLogo from 'src/assets/tf-full-logo.png'
 
 function SupportedFrameworks() {
+    const { ref, inView } = useInView({
+        threshold: 0.5,
+        triggerOnce: true,
+    });
+
+    const fadeInClass = inView ? 'fade-in' : 'hidden';
+
     return (
         <div className="supported-frameworks">
             <a href="https://pytorch.org/" target="_blank" rel="noopener noreferrer">
-                <img src={torchLogo.src} alt="PyTorch" className="torch-logo" />
+                <img src={torchLogo.src} alt="PyTorch" className={`torch-logo ${fadeInClass}`} ref={ref} />
             </a>
             <a href="https://www.tensorflow.org/" target="_blank" rel="noopener noreferrer">
-                <img src={tfLogo.src} alt="TensorFlow" className="tf-logo" />
+                <img src={tfLogo.src} alt="TensorFlow" className={`tf-logo ${fadeInClass}`} ref={ref} />
             </a>
             <a href="https://jax.readthedocs.io/en/latest/notebooks/quickstart.html" target="_blank" rel="noopener noreferrer">
-                <img src={jaxLogo.src} alt="JAX" className="jax-logo" />
+                <img src={jaxLogo.src} alt="JAX" className={`jax-logo ${fadeInClass}`} ref={ref} />
             </a>
             <a href="https://github.com/ml-explore/mlx" target="_blank" rel="noopener noreferrer">
-                <img src={mlxLogo.src} alt="MLX" className="mlx-logo" />
+                <img src={mlxLogo.src} alt="MLX" className={`mlx-logo ${fadeInClass}`} ref={ref} />
             </a>
             <a href="https://numpy.org/" target="_blank" rel="noopener noreferrer">
-                <img src={numpyLogo.src} alt="Numpy" className="numpy-logo" />
+                <img src={numpyLogo.src} alt="Numpy" className={`numpy-logo ${fadeInClass}`} ref={ref} />
             </a>
         </div>
     );
