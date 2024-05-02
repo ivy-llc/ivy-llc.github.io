81cdf26026f3502c000f33742b3a4e33744f7e98
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-02 12:43:39 +0100

    add template login and signup pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useState } from 'react';
+import 'src/styles/App.css';
+import 'src/styles/index.css';
+import 'src/styles/LandingPage.css';
+import 'src/styles/Login.css';
+import NavBar from 'src/components/NavBar';
+import SocialHandles from 'src/components/SocialHandles';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle login logic here
+    };
+
+    return (
+        <>
+            <NavBar />
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit} className="login-form">
+                <label>
+                    Email:
+                    <input className="input-field" type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                </label>
+                <label>
+                    Password:
+                    <input className="input-field" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                </label>
+                <input className="login-button" type="submit" value="Log In" />
+            </form>
+            <SocialHandles />
+        </>
+    );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useState } from 'react';
+import 'src/styles/App.css';
+import 'src/styles/index.css';
+import 'src/styles/LandingPage.css';
+import 'src/styles/Login.css';
+import NavBar from 'src/components/NavBar';
+import SocialHandles from 'src/components/SocialHandles';
+
+export default function Signup() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Handle login logic here
+    };
+
+    return (
+        <>
+            <NavBar />
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSubmit} className="login-form">
+                <label>
+                    Email:
+                    <input className="input-field" type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+                </label>
+                <label>
+                    Password:
+                    <input className="input-field" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+                </label>
+                <input className="login-button" type="submit" value="Sign Up" />
+            </form>
+            <SocialHandles />
+        </>
+    );
+}
