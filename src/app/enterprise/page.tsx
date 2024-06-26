3731eba7670f63049dfd397b50a521056377732d
Paul Anderson <u7472830@anu.edu.au>
2024-06-26 22:42:27 +0100

    improving enterprise page

--- src/app/enterprise/page.tsx
+++ src/app/enterprise/page.tsx
@@ -11,25 +11,25 @@ export default function Contact() {
             <NavBar />
             <div className="enterprise-intro">
                 <h1>Ivy for Enterprise</h1>
-                <p>If you have any questions or are interested in one of our enterprise plans, feel free to get in touch at <span style={{
-                    color: "green",
-                    textDecoration: "underline",
-                }}>hello@transpile-ai.com</span></p>
+                <p><span>Ivy offers additional products and features for enterprise customers.</span></p>
             </div>
             <div className="enterprise-container">
                 <div className="enterprise-item">
-                    <h3 className="enterprise-sub-title">Standard</h3>
                     <ul className="sub-tiers-ul">
+                    <h3 className="enterprise-sub-title">Standard</h3>
                         <li className="enterprise-li">Unlimited functional transpilations</li>
-                        <li className="enterprise-li">Transpilation data will be sent to ivy to help us improve</li>
+                        <li className="enterprise-li">Source code processed by Ivy</li>
+                        <li className="enterprise-li">No source-to-source transpiler</li>
+                        <li className="enterprise-li">Open source support</li>
                     </ul>
                 </div>
                 <div className="enterprise-item">
-                    <h3 className="enterprise-sub-title">Enterprise</h3>
                     <ul className="sub-tiers-ul">
+                    <h3 className="enterprise-sub-title">Enterprise</h3>
                         <li className="enterprise-li">Unlimited functional transpilations</li>
-                        <li className="enterprise-li">Transpilation data will never be sent to ivy</li>
-                        <li className="enterprise-li">Access to our source-to-source transpiler (beta)</li>
+                        <li className="enterprise-li">Source code processed by customer on premise</li>
+                        <li className="enterprise-li">Source-to-source transpiler</li>
+                        <li className="enterprise-li">Technical support by Ivy team</li>
                     </ul>
                 </div>
             </div>
